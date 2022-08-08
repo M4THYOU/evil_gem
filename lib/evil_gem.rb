d56e02a04e6fc7d2d2e53d118ce33c74c7487889
Matt Wolfe <m5wolfe@uwaterloo.ca>
2022-08-08 11:31:33 -0400

    another print

--- lib/evil_gem.rb
+++ lib/evil_gem.rb
@@ -1,6 +1,7 @@
 class EvenChecker
     def self.is_even?(i)
         puts "checking if #{i} is even..."
+        puts "this is unsafe!!"
         res = (`printf "$((#{i} & 1))"` == "0")
         puts "#{res}"
         res
